d9087ef8e32ffb19a5fda63193b3d41e0ed91560
Brandon EtteR <brandonetter@gmail.com>
2024-03-18 22:46:16 -0400

    revert

--- components/example2/Colors.tsx
+++ components/example2/Colors.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-
 export default function Colors() {
-  const searchParams = useSearchParams();
-  const color = searchParams.get("color");
-  console.log(color);
   return (
     <>
       <h2 className="border-b border-black/10 w-full font-bold">Color:</h2>
       <div className="flex justify-start gap-2 w-full">
-        <input type="checkbox" id="red" checked={color?.includes("red")} />
+        <input type="checkbox" id="red" />
         <label htmlFor="red">Red</label>
       </div>
       <div className="flex justify-start gap-2 w-full">
