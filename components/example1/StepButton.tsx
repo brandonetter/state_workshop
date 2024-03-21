25e1952ef8a31ad83208129f286d1843e515d28e
Brandon EtteR <brandonetter@gmail.com>
2024-03-21 09:42:00 -0400

    placeholders

--- components/example1/StepButton.tsx
+++ components/example1/StepButton.tsx
@@ -1,13 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+export function NextButton({ step }: { step: number }) {}
 
-export function NextButton({ step }: { step: number }) {
-  const { push } = useRouter();
-  return <button onClick={() => push(`?step=${step + 1}`)}>Next</button>;
-}
-
-export function BackButton({ step }: { step: number }) {
-  const { push } = useRouter();
-  return <button onClick={() => push(`?step=${step - 1}`)}>Back</button>;
-}
+export function BackButton({ step }: { step: number }) {}
